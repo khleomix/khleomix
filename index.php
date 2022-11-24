02cdf39435372a8b657ac862ed186f8110ba2324
JC <jc@webdevstudios.com>
2022-11-25 02:52:06 +0800

    Add temporary landing page

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="initial-scale=1.0, width=device-width" maximum-scale="1">
+		<title>Khleomix</title>
+		<meta name="next-head-count" content="3">
+		<link rel="preload" href="./style.css" as="style">
+		<link rel="stylesheet" href="./style.css">
+	</head>
+
+    <body>
+        <div class="container">
+            <div class="container__item landing-page-container">
+                <div class="content__wrapper">
+
+                    <header class="header">
+                        <div class="menu-icon header__item">
+                            <span class="menu-icon__line"></span>
+                        </div>
+
+                        <h1 class="heading">..    -.-. --- -.. .    ... ---    -.-- --- ..-    -.. --- -. .----. -    .... .- ...- .    - --- .-.-.-</h1>
+                    </header>
+
+                    <p class="coords">10.7202° N, 122.5621° E"</p>
+
+                    <div class="ellipses-container">
+
+                        <h2 class="greeting">JC Palmes</h2>
+                        <p class="sub-text">Coffee. WordPress. Unicorns.</p>
+
+                        <div class="ellipses ellipses__outer--thin">
+
+                            <div class="ellipses ellipses__orbit"></div>
+
+                        </div>
+
+                        <div class="ellipses ellipses__outer--thick"></div>
+                    </div>
+
+                    <div class="scroller">
+                        <p class="page-title">home</p>
+
+                        <div class="timeline">
+                            <span class="timeline__unit"></span>
+                            <span class="timeline__unit timeline__unit--active"></span>
+                            <span class="timeline__unit"></span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </body>
+</html>
