dcb629faed0c24b1ddf3caa276b222a97e8002ee
JC <jc@webdevstudios.com>
2022-11-26 04:57:37 +0800

    Update mobile styles

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="initial-scale=1.0, width=device-width" maximum-scale="1">
-		<title>Khleomix - JC Palmes</title>
+		<title>Khleomix - JC Palmes - Web Developer</title>
 		<meta name="next-head-count" content="3">
 		<link rel="preload" href="./style.css" as="style">
 		<link rel="stylesheet" href="./style.css">
+        <link rel="shortcut icon" href="./images/unicorn.svg" type="image/x-icon">
+        <link rel="icon" href="./images/unicorn.svg" type="image/x-icon">
 	</head>
 
     <body>
@@ -14,42 +16,53 @@
                 <div class="content-wrapper">
 
                     <header class="header">
-                        <div class="menu-icon header-item hide">
-                            <span class="menu-icon-line"></span>
-                        </div>
-
                         <h1 class="heading">..    -.-. --- -.. .    ... ---    -.-- --- ..-    -.. --- -. .----. -    .... .- ...- .    - --- .-.-.-</h1>
                     </header>
 
-                    <p class="coordinates">10.7202° N, 122.5621° E"</p>
+                    <main class="main">
+                        <p class="coordinates">10.7202° N, 122.5621° E"</p>
 
-                    <div class="ellipses-container">
+                        <div class="ellipses-container">
 
-                        <h2 class="greeting">JC Palmes</h2>
-                        <p class="sub-text">Coffee Unicorns WordPress</p>
+                            <h2 class="greeting">JC Palmes</h2>
+                            <p class="sub-text">Coffee Unicorns WordPress</p>
 
-                        <div class="ellipses ellipses-outer--thin">
+                            <div class="ellipses ellipses-outer--thin">
 
-                            <div class="ellipses ellipses-orbit">
-<div class="cup">
-  <div class="coffee"></div>
-</div>
-<div class="smoke"></div>
+                                <div class="ellipses ellipses-orbit">
+                                    <div class="cup">
+                                        <div class="coffee"></div>
+                                    </div>
+                                    <div class="smoke"></div>
+                                </div>
+                            </div>
+
+                            <div class="ellipses ellipses-outer--thick"></div>
+                        </div>
+
+                        <div class="scroller">
+                            <p class="page-title">home</p>
+
+                            <div class="timeline">
+                                <span class="timeline-unit"></span>
+                                <span class="timeline-unit timeline-unit--active"></span>
+                                <span class="timeline-unit"></span>
                             </div>
                         </div>
+                    </main>
 
-                        <div class="ellipses ellipses-outer--thick"></div>
-                    </div>
-
-                    <div class="scroller">
-                        <p class="page-title">home</p>
-
-                        <div class="timeline">
-                            <span class="timeline-unit"></span>
-                            <span class="timeline-unit timeline-unit--active"></span>
-                            <span class="timeline-unit"></span>
+                    <footer class="footer">
+                       <div class="socialbtns">
+                            <ul>
+                                <li><a href="https://github.com/khleomix/" class="fa fa-lg fa-github" target="_blank"></a></li>
+                                <li><a href="https://profiles.wordpress.org/khleomix/" class="fa fa-lg fa-wordpress" target="_blank"></a></li>
+                                <li><a href="https://www.linkedin.com/in/jcmaepalmes/" class="fa fa-lg fa-linkedin" target="_blank"></a></li>
+                                <li><a href="https://www.facebook.com/jcpalmes/" class="fa fa-lg fa-facebook" target="_blank"></a></li>
+                                <li><a href="https://twitter.com/jpalmes" class="fa fa-lg fa-twitter" target="_blank"></a></li>
+                                <li><a href="https://www.instagram.com/khleomix/" class="fa fa-lg fa-instagram" target="_blank"></a></li>
+                            </ul>
                         </div>
-                    </div>
+                    </footer>
                 </div>
 
             </div>
